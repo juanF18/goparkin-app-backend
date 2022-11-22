c84d0d6c638157e93b36df88a05ff0b1b3774423
usuario <juan.1701821725@ucaldas.edu.co>
2022-11-22 09:53:03 -0500

    Reservation model, migration, controller
    Se crearon los archivos para reserva.
    No se ha hecho migration run, solo se creó la migración

--- database/migrations/1669062717440_reservations.ts
+++ database/migrations/1669062717440_reservations.ts
@@ -6,20 +6,20 @@ export default class extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.integer("idUser").unsigned()
-      table
-      .foreign("idUser")
-      .references("user.id")
-      .onDelete("cascade")
-      /**
-       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
-       */
-       table
-       .foreign("idUser")
-       .references("user.id")
-       .onDelete("cascade")
-      table.timestamp('created_at', { useTz: true })
-      table.timestamp('updated_at', { useTz: true })
+      // table.integer("idUser").unsigned()
+      // table
+      // .foreign("idUser")
+      // .references("user.id")
+      // .onDelete("cascade")
+      //  table
+      //  .foreign("idUser")
+      //  .references("user.id")
+      //  .onDelete("cascade")
+      //las llaves foraneas todavia no estan
+      // faltan los campos idUser y idParking
+      table.date("date")
+      table.string("status",20)
+      table.integer("hour")
     })
   }
 
