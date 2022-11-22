11203e977f6e8c07e9c40118926a06158634549f
Juan Sebastian Giraldo Trujillo <67327495+sebasgiraldo1123@users.noreply.github.com>
2022-11-22 11:56:05 -0500

    Base de los archivos de migración, modelo y controlador de Person
    Squash de los commits, de la rama JS-Person

---
+++ app/Models/Person.ts
@@ -0,0 +1,28 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class Person extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public name: string;
+
+  @column()
+  public lastName: string;
+
+  @column()
+  public phone: string;
+
+  @column()
+  public email: string;
+
+  @column()
+  public password: string;
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
