70924542e62c325aa7d1be1ac0f57d3af3229eec
Juan Felipe Cortes <juanfelipecortescastrillon@gmail.com>
2022-11-24 17:03:51 -0500

    Solucion de identacion

--- app/Controllers/Http/RaitingsController.ts
+++ app/Controllers/Http/RaitingsController.ts
@@ -1,48 +1,46 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import Raiting from 'App/Models/Raiting';
+import Raiting from 'App/Models/Raiting'
 
 export default class RaitingsController {
+  /**
+   * Lista todos los usuarios
+   */
+  public async index(ctx: HttpContextContract) {
+    let raitings: Raiting[] = await Raiting.query()
+    return raitings
+  }
+  /**
+   * Almacena la información de un usuario
+   */
+  public async store({ request }: HttpContextContract) {
+    const body = request.body()
+    const raiting: Raiting = await Raiting.create(body)
+    return raiting
+  }
+  /**
+   * Muestra la información de un solo usuario
+   */
+  public async show({ params }: HttpContextContract) {
+    let raiting = await Raiting.query().where('id', params.id)
+    return raiting
+  }
+  /**
+   * Actualiza la información de un usuario basado
+   * en el identificador y nuevos parámetros
+   */
+  public async update({ params, request }: HttpContextContract) {
+    const body = request.body()
+    const raiting = await Raiting.findOrFail(params.id)
+    raiting.raiting = body.raiting
+    raiting.comment = body.comment
 
-/**
-* Lista todos los usuarios
-*/
-public async index(ctx:HttpContextContract){
-    let raitings:Raiting[] = await Raiting.query()
-        return raitings;
-    }
-    /**
-    * Almacena la información de un usuario
-    */
-    public async store({request}:HttpContextContract){
-        const body= request.body()
-        const raiting: Raiting = await Raiting.create(body)
-        return raiting
-    }
-    /**
-    * Muestra la información de un solo usuario
-    */
-    public async show({params}:HttpContextContract) {
-        let raiting = await Raiting.query().where('id',params.id)
-        return raiting
-    }
-    /**
-    * Actualiza la información de un usuario basado
-    * en el identificador y nuevos parámetros
-    */
-    public async update({params,request}:HttpContextContract) {
-    const body=request.body();
-    const the_raiting=await Raiting.findOrFail(params.id);
-    the_raiting.raiting=body.raiting;
-    the_raiting.comment=body.comment;
-    
-    return the_raiting.save();
-    }
-    /**
-    * Elimina a un usuario basado en el identificador
-    */
-    public async destroy({params}:HttpContextContract) {
-    const the_raiting=await Raiting.findOrFail(params.id);
-    return the_raiting.delete();
-    }
-
+    return raiting.save()
+  }
+  /**
+   * Elimina a un usuario basado en el identificador
+   */
+  public async destroy({ params }: HttpContextContract) {
+    const raiting = await Raiting.findOrFail(params.id)
+    return raiting.delete()
+  }
 }
