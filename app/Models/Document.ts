b122b4aafc596690e94ba664a73bf4ded1010d76
Juan Felipe Cortes <juanfelipecortescastrillon@gmail.com>
2022-11-24 18:49:47 -0500

    Se agrega la realciones entre los modelos

--- app/Models/Document.ts
+++ app/Models/Document.ts
@@ -1,9 +1,17 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, BelongsTo, belongsTo, column } from '@ioc:Adonis/Lucid/Orm'
+import Admin from './Admin'
+import Parking from './Parking'
 
 export default class Document extends BaseModel {
   @column({ isPrimary: true })
   public id: number
+
+  @column()
+  public id_admin: number
+
+  @column()
+  public id_parking: number
 
   @column()
   public url: string
@@ -19,4 +27,9 @@ export default class Document extends BaseModel {
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
+
+  @belongsTo(() => Admin, {
+    foreignKey: 'id_admin',
+  })
+  public admin: BelongsTo<typeof Admin>
 }
--- app/Models/Parking.ts
+++ app/Models/Parking.ts
@@ -1,9 +1,27 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import {
+  BaseModel,
+  BelongsTo,
+  belongsTo,
+  column,
+  HasMany,
+  hasMany,
+  HasOne,
+  hasOne,
+} from '@ioc:Adonis/Lucid/Orm'
+import Document from './Document'
+import Adress from './Adress'
+import ParkingSpace from './ParkingSpace'
+import Reservation from './Reservation'
+import Raiting from './Raiting'
+import Owner from './Owner'
 
 export default class Parking extends BaseModel {
   @column({ isPrimary: true })
   public id: number
+
+  @column()
+  public id_owner: number
 
   @column()
   public parking_name: string
@@ -22,4 +40,52 @@ export default class Parking extends BaseModel {
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
+
+  // pertenece a
+  @belongsTo(() => Owner, {
+    foreignKey: 'id_owner',
+  })
+  public owner: BelongsTo<typeof Owner>
+  // Relaciones 1 a 1
+  /**
+   * Un parqueadero tiene un docuemnto
+   */
+  @hasOne(() => Document, {
+    foreignKey: 'id_parking',
+  })
+  public document: HasOne<typeof Document>
+
+  /**
+   * Un parqueadero tiene una direccion
+   */
+  @hasOne(() => Adress, {
+    foreignKey: 'id_parking',
+  })
+  public adress: HasOne<typeof Adress>
+
+  /**
+   * Un parqueadero tiene una direccion
+   */
+  @hasOne(() => ParkingSpace, {
+    foreignKey: 'id_parking',
+  })
+  public parkingSpace: HasOne<typeof ParkingSpace>
+
+  // Relacion 1 a N
+
+  /**
+   * Un parqueadero tiene muchas reservaciones
+   */
+  @hasMany(() => Reservation, {
+    foreignKey: 'id_parking',
+  })
+  public reservations: HasMany<typeof Reservation>
+
+  /**
+   * Un parqueadero tiene muchas reservaciones
+   */
+  @hasMany(() => Raiting, {
+    foreignKey: 'id_parking',
+  })
+  public raitings: HasMany<typeof Raiting>
 }
