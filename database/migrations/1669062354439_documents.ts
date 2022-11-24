b4711cabb66ebb5ee0706ac8fb9e40e8d10ea944
Juan Felipe Cortes <juanfelipecortescastrillon@gmail.com>
2022-11-24 15:59:47 -0500

    Se agrega relaciones 1 a 1

--- database/migrations/1669062354439_documents.ts
+++ database/migrations/1669062354439_documents.ts
@@ -6,6 +6,7 @@ export default class extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
+      table.integer('id_parking').unsigned().references('parkings.id').onDelete('CASCADE')
       table.string('url', 400).notNullable()
       table.string('comment', 400).notNullable()
       table.string('status', 100).notNullable()
