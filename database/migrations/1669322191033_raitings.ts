f89de8cf7d3feb382343393e3d6c0803fff33eba
Juan Felipe Cortes <juanfelipecortescastrillon@gmail.com>
2022-11-24 17:03:51 -0500

    Solucion de identacion

--- database/migrations/1669322191033_raitings.ts
+++ database/migrations/1669322191033_raitings.ts
@@ -3,11 +3,11 @@ import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 export default class extends BaseSchema {
   protected tableName = 'raitings'
 
-  public async up () {
+  public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
       table.integer('raiting').notNullable()
-      table.string('comment',800).notNullable()
+      table.string('comment', 800).notNullable()
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
        */
@@ -16,7 +16,7 @@ export default class extends BaseSchema {
     })
   }
 
-  public async down () {
+  public async down() {
     this.schema.dropTable(this.tableName)
   }
 }
