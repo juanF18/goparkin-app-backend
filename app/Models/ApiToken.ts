0ed034008c1ed319e35fa3576315be369350f8f3
Heidy12 <95388865+Heidy12@users.noreply.github.com>
2022-12-13 14:16:48 -0500

    token y segurity

---
+++ app/Models/ApiToken.ts
@@ -0,0 +1,33 @@
+import { DateTime } from 'luxon'
+import { BaseModel, BelongsTo, belongsTo, column } from '@ioc:Adonis/Lucid/Orm'
+import Person from './Person'
+
+export default class ApiToken extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public id_person: number
+
+  @column()
+  public name: string
+ 
+  @column()
+  public type: string
+ 
+  @column()
+  public token: string
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  @belongsTo(()=> Person, {
+    localKey: 'id_person',
+  })
+public rol: BelongsTo<typeof Person>
+
+
+}
--- app/Models/Person.ts
+++ app/Models/Person.ts
@@ -1,9 +1,14 @@
 import { DateTime } from 'luxon'
-import { BaseModel, BelongsTo, belongsTo, column, HasMany, hasMany } from '@ioc:Adonis/Lucid/Orm'
-import Owner from './Owner'
-import User from './User'
-import Admin from './Admin'
-import Rol from './Rol'
+import { BaseModel, beforeSave, BelongsTo, belongsTo, column, HasMany, hasMany } from '@ioc:Adonis/Lucid/Orm'
+// import Owner from './Owner'
+// import User from './User'
+// import Admin from './Admin'
+import Rol from './Rol';
+import ApiToken from './ApiToken';
+import Hash from '@ioc:Adonis/Core/Hash'
+
+
+
 
 export default class Person extends BaseModel {
   @column({ isPrimary: true })
@@ -41,27 +46,39 @@ export default class Person extends BaseModel {
   })
   public rol: BelongsTo<typeof Rol>
 
+  @hasMany (() => ApiToken,{
+    foreignKey: 'id_person',
+  })
+  public persons:HasMany<typeof ApiToken>
+
+  @beforeSave()
+  public static async hashPassword (the_person: Person) {
+    if (the_person.$dirty.password) {
+      the_person.password = await Hash.make(the_person.password)
+    }
+  }
+}
   /**
    * Personas tiene muchos dueños
    */
-  @hasMany(() => Owner, {
-    foreignKey: 'id_person',
-  })
-  public owners: HasMany<typeof Owner>
+  // @hasMany(() => Owner, {
+  //   foreignKey: 'id_person',
+  // })
+  // public owners: HasMany<typeof Owner>
 
   /**
    * Personas tiene muchos usuarios
    */
-  @hasMany(() => User, {
-    foreignKey: 'id_person',
-  })
-  public users: HasMany<typeof User>
+  // @hasMany(() => User, {
+  //   foreignKey: 'id_person',
+  // })
+  // public users: HasMany<typeof User>
 
   /**
    * Personas tiene muchos admins
    */
-  @hasMany(() => Admin, {
-    foreignKey: 'id_person',
-  })
-  public admins: HasMany<typeof Admin>
-}
+//   @hasMany(() => Admin, {
+//     foreignKey: 'id_person',
+//   })
+//   public admins: HasMany<typeof Admin>
+
